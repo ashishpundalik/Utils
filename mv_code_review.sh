c791a3f115f5ca14659df7d9c9be8d0e893f2c1d
Aashish Pundalik <aashish@jiva.ag>
2021-06-22 10:16:43 +0800

    Add support for git bundle. Extract into individual functions.

--- mv_code_review.sh
+++ mv_code_review.sh
@@ -16,14 +16,27 @@ mkdir -p $CODE_REVIEW_PATH/$1/$2
 
 if [[ "$3" = *.zip ]]
 then
+  move_zip
+elif [[ $3 == http://* ]] || [[ $3 == https://* ]] || [[ "$3" = *.bundle ]]
+then
+  clone_git
+else
+  move_file
+fi
+
+move_zip() {
   echo "unzipping $3..."
   unzip $CODE_DOWNLOAD_PATH/$3 -d $CODE_REVIEW_PATH/$1/$2/
   rm $CODE_DOWNLOAD_PATH/$3
-elif [[ $3 == http://* ]] || [[ $3 == https://* ]]
-then
+}
+
+clone_git() {
   echo "cloning $3..."
   git clone $3 $CODE_REVIEW_PATH/$1/$2/
-else
+  rm $CODE_DOWNLOAD_PATH/$3
+}
+
+move_file() {
   echo "moving $3..."
   mv $CODE_DOWNLOAD_PATH/$3 $CODE_REVIEW_PATH/$1/$2/
-fi
+}

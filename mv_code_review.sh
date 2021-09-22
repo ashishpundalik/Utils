1a79a6d36a011400e9360a7ef5b3b91b48a2b084
Aashish Pundalik <aashish@jiva.ag>
2021-09-22 11:34:19 +0800

    Add implementation for git bundles

--- mv_code_review.sh
+++ mv_code_review.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-echo "Code Download Path: "
-echo $CODE_DOWNLOAD_PATH
+echo "Code Download Path: $CODE_DOWNLOAD_PATH"
 
-echo "Code Review Path: "
-echo $CODE_REVIEW_PATH
+echo "Code Review Path: $CODE_REVIEW_PATH"
+
+move_zip() {
+  echo "unzipping $2..."
+  unzip $CODE_DOWNLOAD_PATH/$2 -d $CODE_REVIEW_PATH/$1
+  rm $CODE_DOWNLOAD_PATH/$2
+}
+
+clone_git() {
+  echo $CODE_DOWNLOAD_PATH/$2
+  file_name=$2
+  extention=".bundle"
+  blank=""
+  assignment_name="${file_name/$extention/$blank}"
+  clone_dest=$CODE_REVIEW_PATH/$1/$assignment_name
+  echo "Cloning at Path: $CODE_REVIEW_PATH/$1/$assignment_name"
+  git clone $CODE_DOWNLOAD_PATH/$2 $clone_dest
+  rm $CODE_DOWNLOAD_PATH/$2
+}
+
+move_file() {
+  echo "moving $2..."
+  mv $CODE_DOWNLOAD_PATH/$2 $CODE_REVIEW_PATH/$1
+}
 
 
 if [[ -z "$CODE_DOWNLOAD_PATH" ]] || [[ -z "$CODE_REVIEW_PATH" ]]; then
@@ -12,31 +33,14 @@ if [[ -z "$CODE_DOWNLOAD_PATH" ]] || [[ -z "$CODE_REVIEW_PATH" ]]; then
   exit 1
 fi
 
-mkdir -p $CODE_REVIEW_PATH/$1/$2
+mkdir -p $CODE_REVIEW_PATH/$1
 
 if [[ "$2" = *.zip ]]
 then
-  move_zip
+  move_zip $1 $2
 elif [[ $2 == http://* ]] || [[ $2 == https://* ]] || [[ "$2" = *.bundle ]]
 then
-  clone_git
+  clone_git $1 $2
 else
-  move_file
+  move_file $1 $2
 fi
-
-move_zip() {
-  echo "unzipping $2..."
-  unzip $CODE_DOWNLOAD_PATH/$2 -d $CODE_REVIEW_PATH/$1/
-  rm $CODE_DOWNLOAD_PATH/$2
-}
-
-clone_git() {
-  echo "cloning $2..."
-  git clone $2 $CODE_REVIEW_PATH/$1/
-  rm $CODE_DOWNLOAD_PATH/$2
-}
-
-move_file() {
-  echo "moving $2..."
-  mv $CODE_DOWNLOAD_PATH/$2 $CODE_REVIEW_PATH/$1/
-}
